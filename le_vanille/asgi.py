31d05e3385837d5adc206a5aab315ecf66fd1720
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-02-06 21:27:05 +0500

    Initial commit

---
+++ le_vanille/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for le_vanille project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'le_vanille.settings')
+
+application = get_asgi_application()
